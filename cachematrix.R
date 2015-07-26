c6e3b4b020c5b99a01a8455c215fecf19c1a07b5
Frank Warf <coursera1999@gmail.com>
2015-07-25 20:17:03 -0400

    Program Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#The purpose of the makecacheMatrix is to create a special object that creates
+#and contains a matrix, its inverse and the functions to get and set those.
 
 makeCacheMatrix <- function(x = matrix()) {
-            m<-NULL
+            Inv_X<-NULL
             set<-function(y){
                         x<<-y
-                        m<<-NULL
-                        
-            
+                        Inv_X<-NULL
             }
             get<-function() x
-            setmatrix<-function(solve) m<<- solve
-            getmatrix<-function() m
+            setmatrix<-function(solve) Inv_X<<- solve
+            getmatrix<-function() Inv_X
             list(set=set, get=get,
                  setmatrix=setmatrix,
                  getmatrix=getmatrix)
@@ -21,22 +18,26 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
+#In cacheSolve, the purpose is to calculate the inverse of the matrix for an
+#object created by "makeCacheMatrix", if it has not previously done so. If cacheSolve
+#calculates the inverse of the matrix, it will cache the inverse via setmatrix
+#and returns the inverse using Inv_X.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-
-            m<-x$getmatrix()
-            if(!is.null(m)){
+      
+            Inv_X<-x$getmatrix()
+            if(!is.null(Inv_X)){
                         message("getting cached data")
-                        return(m)
+                        return(Inv_X)
             }
             matrix<-x$get()
-            m<-solve(matrix, ...)
-            x$setmatrix(m)
-            m
+            Inv_X<-solve(matrix, ...)
+            x$setmatrix(Inv_X)
+            Inv_X
              
 }
+
+
 
 
 
